51ccd6e41212caba350319950dd9e4f5299e433e
Aakarshit07 <aakarshit070@gmail.com>
2024-06-20 01:06:52 +0530

    add array basics

---
+++ 04arrays/myArray.ts
@@ -0,0 +1,20 @@
+const superHeros: string[] = [];
+const heroPower: Array<number> = [];
+
+type User = {
+    name: string;
+    isActive: boolean
+}
+
+const allUser: User[] = [];
+
+superHeros.push("spiderman");
+heroPower.push(100);
+
+allUser.push({name: "", isActive: true});
+
+
+const MLModels: number[][] = [
+    [255, 255, 255],
+    []
+]
