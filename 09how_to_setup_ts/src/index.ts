9d8be5f6e7e71bcc232c5f44aeb8f82cb3d52db0
Aakarshit07 <aakarshit070@gmail.com>
2024-06-23 00:33:01 +0530

    Add 10Class

--- 09how_to_setup_ts/src/index.ts
+++ 09how_to_setup_ts/src/index.ts
@@ -1,2 +1,18 @@
-console.log("typescript is here still");
-console.log("typescript is amazing");
+// 10class part
+class User {
+    email: string
+    name: string
+    readonly city: string = "Delhi"
+    constructor(email: string, name: string) {
+    //here to fix email we use inform typescrip before constructors
+        this.email = email;
+        this.name = name;
+    }
+}
+
+//! Don't do this
+// const hitesh = new User(email: "a@a.com", name: "soul")
+
+// Can do this
+const soul = new User("a@a.com", "soul")
+// console.log(soul);
