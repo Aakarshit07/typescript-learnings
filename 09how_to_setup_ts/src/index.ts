4e02fb3f7774398980eaab46262c43dd4af200aa
Aakarshit07 <aakarshit070@gmail.com>
2024-06-23 01:01:13 +0530

    Add 11 private and public

--- 09how_to_setup_ts/src/index.ts
+++ 09how_to_setup_ts/src/index.ts
@@ -1,18 +1,34 @@
 // 10class part
-class User {
-    email: string
-    name: string
-    readonly city: string = "Delhi"
-    constructor(email: string, name: string) {
-    //here to fix email we use inform typescrip before constructors
-        this.email = email;
-        this.name = name;
-    }
-}
+
+// class User {
+//     public email: string //everything at default public marked as public and we can mark it as well.
+//     name: string
+//     private readonly city: string = "Delhi" // suppose dont wwant any one to be able to access it
+//     constructor(email: string, name: string) {
+//     //here to fix email type we use to inform typescript before constructors
+//         this.email = email;
+//         this.name = name;
+//     }
+// }
 
 //! Don't do this
 // const hitesh = new User(email: "a@a.com", name: "soul")
 
 // Can do this
-const soul = new User("a@a.com", "soul")
-// console.log(soul);
+// const soul = new User("a@a.com", "soul")
+// soul.email
+
+
+
+/*Professional way*/
+// 11Private and public property
+
+class User {
+    readonly city: string = "Delhi"
+    constructor(
+        public email: string, 
+        public name: string,
+        private userId: string
+        ){   
+    }
+}
