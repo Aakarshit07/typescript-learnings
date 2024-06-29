ef1653487023d8f332fb54afca24cd90a772092c
Aakarshit07 <aakarshit070@gmail.com>
2024-06-29 22:35:37 +0530

    add instance of

--- 09how_to_setup_ts/src/detectionOfType.ts
+++ 09how_to_setup_ts/src/detectionOfType.ts
@@ -1,55 +1,85 @@
 function detectType(val: number | string) {
-    if (typeof val === "string") {
-       return val.toLowerCase();
-    } 
-    return val + 3;
+  if (typeof val === "string") {
+      return val.toLowerCase();
+  } 
+  return val + 3;
 
-    //what if we have more than one type like and number array.
+  //what if we have more than one type like and number array.
 }
 
 function provedeId(id: string | null) {
-    if(!id) {
-        console.log("Please provide ID");
-        return;
-    }
-    id?.toLowerCase();
+  if(!id) {
+      console.log("Please provide ID");
+      return;
+  }
+  id?.toLowerCase();
 }
 
 
 function printAll(strs: string | string[] | null) {
-    // !!!!!!!!!!!!!!!!
-    //  DON'T DO THIS!
-    //   KEEP READING
-    // !!!!!!!!!!!!!!!!
-    if (strs) {
-      if (typeof strs === "object") {
-        for (const s of strs) {
-          console.log(s);
-        }
-      } else if (typeof strs === "string") {
-        console.log(strs);
+  // !!!!!!!!!!!!!!!!
+  //  DON'T DO THIS!
+  //   KEEP READING
+  // !!!!!!!!!!!!!!!!
+  if (strs) {
+    if (typeof strs === "object") {
+      for (const s of strs) {
+        console.log(s);
       }
+    } else if (typeof strs === "string") {
+      console.log(strs);
     }
+  }
 }
 
 
 interface User {
-    name: string,
-    email: string
+  name: string,
+  email: string
 }
 
 interface Admin {
-    name: string,
-    email: string,
-    isAdmin: boolean
+  name: string,
+  email: string,
+  isAdmin: boolean
 }
 
 function isAdminAccount(account: User | Admin) {
-    // we can't return admin becauser the propert is in admin or not
+  // we can't return admin becauser the propert is in admin or not
     
-    // here we checking if isAdmin in accound or not
-    if("isAdmin" in account) {
-        return account.isAdmin
-    }
+  // here we checking if isAdmin in accound or not
+  if("isAdmin" in account) {
+    return account.isAdmin
+  }
+}
 
+
+
+// Anything that uses new keyword 
+// using instanceof can be used to check the instance of and 
+// can mostly be use where there is an new keyword.
+
+function logValue(x: Date | string) {
+  if (x instanceof Date) {
+    console.log(x.toUTCString());
+  } else {
+    console.log(x.toUpperCase());      
+  }
+}
+
+type Fish = {swim: () => void};
+type Bird = {fly: () => void};
+
+function isFish(pet: Fish | Bird): pet is Fish {
+  return (pet as Fish).swim !== undefined;
+}
+
+function getFood(pet: Fish | Bird) {
+  if(isFish(pet)) {
+    pet
+    return "fish food"
+  } else  {
+    pet
+    return "bird food"
+  }
 }
